f5cef0f4ed6d1d7b21155d81a9431b8f8c3190bc
Fabian <f.stiehle@gmail.com>
2022-07-19 13:18:23 +0200

    implement REST API tests

--- state-channel-node/test/begin.test.ts
+++ state-channel-node/test/begin.test.ts
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = 'test';
 
-import express, { Express, Request, Response } from 'express';
+import express from 'express';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import crypto from 'crypto';
@@ -10,17 +10,17 @@ import RoutingInformation from '../src/classes/RoutingInformation';
 import SupplyChainRouting from '../src/classes/SupplyChainRouting';
 import SupplyChainConformance from '../src/classes/SupplyChainConformance';
 import { Server } from 'node:http';
+import { doesNotMatch } from 'assert';
 const {expect} = chai;
 
 chai.use(chaiHttp);
 describe('/begin and /step', () => {
   let keys: Map<Participant, [string, string]>;
-  
-  let manufacturer: Server;
-  let bulkbuyer: Server;
+  let servers: Map<Participant, Server>;
+  let participants: Map<Participant, RoutingInformation>;
 
   before(() => {
-    const participants = new Map<Participant, RoutingInformation>([
+    participants = new Map<Participant, RoutingInformation>([
       [Participant.BulkBuyer, new RoutingInformation(Participant.BulkBuyer, 'localhost', 9001)],
       [Participant.Manufacturer, new RoutingInformation(Participant.Manufacturer, 'localhost', 9002)],
       [Participant.Middleman, new RoutingInformation(Participant.Middleman, 'localhost', 9003)],
@@ -28,6 +28,7 @@ describe('/begin and /step', () => {
       [Participant.SpecialCarrier, new RoutingInformation(Participant.SpecialCarrier, 'localhost', 9005)],
     ]);
     
+    servers = new Map<Participant, Server>();
     keys = new Map<Participant, [string, string]>();
     const pubKeys = new Map<Participant, string>();
     for (const [participant, routingInformation] of participants) {
@@ -49,45 +50,82 @@ describe('/begin and /step', () => {
       routingInformation.pubKey = publicKey;
       keys.set(participant, [publicKey, privateKey]);
       pubKeys.set(participant, publicKey)
+      servers.set(
+        participant, 
+        configureServer(express(), {
+          me: participant,
+          publicKey: publicKey,
+          privateKey: privateKey
+        },
+        new SupplyChainRouting(participants),
+        new SupplyChainConformance(pubKeys)
+        )
+        .listen(routingInformation.port, () => console.log(`${participant} Running on ${routingInformation.port} ⚡`))
+      );
     }
-
-    let [publicKey, privateKey] = keys.get(Participant.Manufacturer);
-    manufacturer = configureServer(express(), {
-        me: Participant.Manufacturer,
-        publicKey: publicKey,
-        privateKey: privateKey
-      },
-      new SupplyChainRouting(participants),
-      new SupplyChainConformance(pubKeys)
-    )
-    .listen(9002, () => console.log(`Manufacturer Running on 9002 ⚡`));
-
-    [publicKey, privateKey] = keys.get(Participant.BulkBuyer);
-    bulkbuyer = configureServer(express(), {
-        me: Participant.BulkBuyer,
-        publicKey: publicKey,
-        privateKey: privateKey
-      },
-      new SupplyChainRouting(participants),
-      new SupplyChainConformance(pubKeys)
-    )
-    .listen(9001, () => console.log(`Bulk Buyer Running on 9001 ⚡`));
   })
 
   after(() => {
-    manufacturer.close();
-    bulkbuyer.close();
+    for (const [_, server] of servers) {
+      server.close();
+    }
   });
 
-  it('test with conforming behaviour', (done) => {
-    
-    chai.request('http://localhost:9001')
+  it('test with conforming behaviour', async () => {
+    // Bulk Buyer to Manufacturer
+    await chai.request('http://localhost:' + participants.get(Participant.BulkBuyer).port)
       .get('/begin/0')
-      .end((err, res) => {
-        expect(err).to.be.null;
+      .then(res => {
         expect(res).to.have.status(200);
-        done();
-      });
-  });
+      })
+      .catch(err => {
+        expect(err).to.be.null;
+        console.log(err);
+     });
+     
 
+    // Manufacturer to Middleman
+    await chai.request('http://localhost:' + participants.get(Participant.Manufacturer).port)
+      .get('/begin/1')
+      .then(res => {
+        expect(res).to.have.status(200);
+      })
+      .catch(err => {
+        expect(err).to.be.null;
+        console.log(err);
+     });
+
+    // Middleman to Supplier
+    await chai.request('http://localhost:' + participants.get(Participant.Middleman).port)
+    .get('/begin/3')
+    .then(res => {
+      expect(res).to.have.status(200);
+    })
+    .catch(err => {
+      expect(err).to.be.null;
+      console.log(err);
+    });
+
+    // Middleman to SpecialCarrier
+    await chai.request('http://localhost:' + participants.get(Participant.Middleman).port)
+    .get('/begin/5')
+    .then(res => {
+      expect(res).to.have.status(200);
+    })
+    .catch(err => {
+      expect(err).to.be.null;
+      console.log(err);
+    });
+
+    // SpecialCarrier to Supplier
+    await chai.request('http://localhost:' + participants.get(Participant.SpecialCarrier).port)
+    .get('/begin/7')
+    .then(res => {
+      expect(res).to.have.status(200);
+    })
+    .catch(err => {
+      expect(err).to.be.null;
+      console.log(err);
+    });
+  });
 });
